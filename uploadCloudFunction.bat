38a7d981789e0206f36c3a9838e450b6dbda4a44
np1139059565 <1139059565@qq.com>
2022-01-01 12:41:17 +0800

    add app file

---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e abcc-f6c81 --n quickstartFunctions --r --project "C:\Users\linchangyi\Desktop\Git\pepper\pepper" && "C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e yfwq1-4nvjm --n quickstartFunctions --r --project "C:\Users\linchangyi\Desktop\Git\pepper\pepper" --report
