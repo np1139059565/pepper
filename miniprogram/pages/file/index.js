35757aae1bd7fb82522c0d91e2499d0591f20fdb
np1139059565 <1139059565@qq.com>
2022-02-21 13:03:08 +0800

    re err loading

--- miniprogram/pages/file/index.js
+++ miniprogram/pages/file/index.js
@@ -106,7 +106,7 @@ Page({
                         this.setData(this.data)
                         this.f_refush_child(true)
                     } else {
-                        app.data.module_log.f_static_show_loading("已经到根目录了！")
+                        app.data.module_log.f_static_show_loading({title:"已经到根目录了！",mask:true})
                     }
                     break;
                 default:
