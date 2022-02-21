3d610175032f74ca28f0f8c140ecb587720960d1
np1139059565 <1139059565@qq.com>
2022-02-21 20:22:15 +0800

    re app.js

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -10,7 +10,7 @@ App({
     try{
       //init log
       this.data.module_log=require("common/wx/mlog.js")
-      this.data.module_db.f_static_show_loading("正在加载。。。")
+      this.data.module_log.f_static_show_loading("正在加载。。。")
       // this.data.module_log.f_static_init(this.data.module_log.f_static_get_log_types().INFO)
       this.f_err=this.data.module_log.f_static_err
       this.f_info=this.data.module_log.f_static_info
@@ -19,7 +19,7 @@ App({
       this.data.module_yun=require("common/wx/myun.js")
       this.data.module_yun.f_static_init()
       this.data.module_db=require("common/wx/local_db.js")
-      this.data.module_db.f_static_init("db_pepper")
+      this.data.module_db.f_static_init("db_pepper",()=>wx.hideLoading)
     }catch (e){
       this.f_err(e)
       this.data.module_log.f_static_show_loading({title:"app.js初始化失败,请退出重新打开试试!",mask:true})
