579f72dfc5ec171ef1a0a48a2dfd5ddda0b2847a
np1139059565 <1139059565@qq.com>
2022-02-21 12:57:56 +0800

    re err loading

--- miniprogram/pages/file/index.js
+++ miniprogram/pages/file/index.js
@@ -106,7 +106,7 @@ Page({
                         this.setData(this.data)
                         this.f_refush_child(true)
                     } else {
-                        app.f_err("is root dir",parent_path)
+                        app.f_static_show_loading("is root dir",parent_path)
                     }
                     break;
                 default:
